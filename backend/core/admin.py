617845e78ef8a23eb493ee5b456736d9cce298de
Maria <hrisca.maria2702@gmail.com>
2026-03-04 13:35:41 +0000

    register models in admin.py + set up django server

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import (
+    User,
+    Society,
+    Membership,
+    Poll,
+    PollOption,
+    PollVote,
+    Review,
+    ReviewResponse,
+    ReviewReaction
+)
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Society)
+admin.site.register(Membership)
+admin.site.register(Poll)
+admin.site.register(PollOption)
+admin.site.register(PollVote)
+admin.site.register(Review)
+admin.site.register(ReviewResponse)
+admin.site.register(ReviewReaction)
