b64a9f76f0cafc796a86e50c2106ed645da644fe
Maria <hrisca.maria2702@gmail.com>
2026-03-04 14:00:44 +0000

    add API viewsets for societies, polls, reviews and votes

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import (
+    Society,
+    Membership,
+    Poll,
+    PollOption,
+    PollVote,
+    Review,
+    ReviewResponse,
+    ReviewReaction
+)
 
-# Create your views here.
+from .serializers import (
+    SocietySerializer,
+    MembershipSerializer,
+    PollSerializer,
+    PollOptionSerializer,
+    PollVoteSerializer,
+    ReviewSerializer,
+    ReviewResponseSerializer,
+    ReviewReactionSerializer
+)
+
+
+class SocietyViewSet(viewsets.ModelViewSet):
+    queryset = Society.objects.all()
+    serializer_class = SocietySerializer
+
+
+class MembershipViewSet(viewsets.ModelViewSet):
+    queryset = Membership.objects.all()
+    serializer_class = MembershipSerializer
+
+
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class PollOptionViewSet(viewsets.ModelViewSet):
+    queryset = PollOption.objects.all()
+    serializer_class = PollOptionSerializer
+
+
+class PollVoteViewSet(viewsets.ModelViewSet):
+    queryset = PollVote.objects.all()
+    serializer_class = PollVoteSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class ReviewResponseViewSet(viewsets.ModelViewSet):
+    queryset = ReviewResponse.objects.all()
+    serializer_class = ReviewResponseSerializer
+
+
+class ReviewReactionViewSet(viewsets.ModelViewSet):
+    queryset = ReviewReaction.objects.all()
+    serializer_class = ReviewReactionSerializer
